3276de7c884f067036e515e2d0ecc2a94c21ccc4
m075 <moemotara@gmail.com>
2026-08-12 17:18:40 +0200

    phase 1 and 2

--- app/dashboard/list-files/page.jsx
+++
@@ -1,9 +0,0 @@
-import FileManager from "@/assets/components/ListFiles";
-
-export default function ListFilesPage() {
-  return (
-    <div className="container mx-auto py-8">
-      <FileManager />
-    </div>
-  );
-}
